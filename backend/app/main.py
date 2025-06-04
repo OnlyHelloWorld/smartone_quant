c03f83bff70ea4298e31e3d14234f7392423bbe7
沈通 <191678946@qq.com>
2025-06-04 17:30:23 +0800

    Auto commit: 2025/06/04 周三 17:30:23.89

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,18 +3,31 @@ from fastapi import FastAPI
 from fastapi.routing import APIRoute
 from starlette.middleware.cors import CORSMiddleware
 
+from app.api.main import api_router
+from app.core.config import settings
+
 
 def custom_generate_unique_id(route: APIRoute) -> str:
-    """
-    参数:
-    - route (APIRoute): FastAPI 路由对象。
-    通过路由的标签和名称生成唯一ID。
-    """
     return f"{route.tags[0]}-{route.name}"
 
+
 if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
-    """
-    初始化 Sentry SDK 以捕获错误和性能数据。
-    仅在非本地环境中启用。
-    """
     sentry_sdk.init(dsn=str(settings.SENTRY_DSN), enable_tracing=True)
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    generate_unique_id_function=custom_generate_unique_id,
+)
+
+# Set all CORS enabled origins
+if settings.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
